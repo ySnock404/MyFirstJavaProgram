f4f2bd97680e2cb561e59c1dc4575e3319923676
ySnock <82345402+ySnock404@users.noreply.github.com>
2021-08-22 13:47:04 +0100

    Add files via upload

---
+++ MyFirstProgram/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I don't know what I'm i doing");
+		System.out.println("I don't really know");
+		
+		// This is a comment
+		
+		/*
+		 * This
+		 * is
+		 * a
+		 * comment
+		 * :)
+		 * 
+		 */
+	}
+
+}
